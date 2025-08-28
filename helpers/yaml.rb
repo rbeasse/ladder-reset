60d74023288856e1ec5df396569c5451715cc5f4
Ryan Beasse <git@ryanbeasse.com>
2025-08-28 11:29:42 -0500

    Added support for releases and backgrounds and added borderlands

--- helpers/yaml.rb
+++ helpers/yaml.rb
@@ -3,3 +3,7 @@ require 'yaml'
 def yaml
   YAML.load_file(File.join(Dir.pwd, 'releases.yml'))
 end
+
+def game_releases
+  yaml['releases'].sort_by { |release| Date.parse(release['time']) }.select { |release| Date.parse(release['time']) >= Date.today - 3 }
+end
