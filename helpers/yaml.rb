cd3660f15bcdd920ffcc22cb4dd01a9803ba1df0
Ryan Beasse <git@ryanbeasse.com>
2025-09-24 10:34:03 -0500

    favicons, metadata and better path of exile information

--- helpers/yaml.rb
+++ helpers/yaml.rb
@@ -9,19 +9,15 @@ def game_releases
 end
 
 def upcoming_releases
-  three_days_ago = Time.now - (3 * 24 * 60 * 60)
-
   game_releases.select do |release|
     release_time = Time.parse(release['time'])
-    release_time >= three_days_ago
+    release_time >= Time.now
   end
 end
 
 def historical_releases
-  three_days_ago = Time.now - (3 * 24 * 60 * 60)
-
   game_releases.select do |release|
     release_time = Time.parse(release['time'])
-    release_time < three_days_ago
+    release_time < Time.now
   end
 end
